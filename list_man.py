866953dec711ec602ac0ca310068399b69fa4914
Abid <abidsharier29@gmail.com>
2025-05-10 00:05:02 -0400

    lists type2

--- list_man.py
+++ list_man.py
@@ -1,4 +1,4 @@
-fname = input("Enter file name: ")
+"""fname = input("Enter file name: ")
 fh = open(fname)
 lst = list()
 for line in fh:
@@ -13,3 +13,20 @@ for line in fh:
     
 lst.sort()
 print(lst)
+"""
+fname = input("Enter file name: ")
+try:
+    fh= open(fname)
+except:
+    print("Not found:",fname)
+    quit()
+count = 0
+for line in fh:
+    if not line.startswith("From "):
+        continue
+    line = line.rstrip()
+    line=line.split()
+    print(line[1])
+    count+=1
+
+print("There were", count, "lines in the file with From as the first word")

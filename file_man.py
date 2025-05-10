4b04cdd5f6d5f61988b80beac9d2da2784034b08
Abid <abidsharier29@gmail.com>
2025-05-09 23:09:48 -0400

    2 solutions

--- file_man.py
+++ file_man.py
@@ -1,3 +1,4 @@
+"""
 fname = input ("Enter file name: ")
 try:
     fh = open (fname)
@@ -7,3 +8,21 @@ except:
 for line in fh:
     line = line.upper()
     print(line.rstrip())
+    
+"""
+
+fname = input("Enter file name: ")
+fh = open(fname)
+addr=0
+count=0
+for line in fh:
+    if not line.startswith("X-DSPAM-Confidence:"):
+        continue
+    #print(line)
+    count+=1
+    line = line.split(" ")
+    num = line[1]
+    #print(num)
+    addr+=float(num)
+ave= addr/count
+print("Average spam confidence: ", ave)

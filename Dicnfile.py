73f0ae91d81b55444c3c0db24b727aef61c7f6c2
Abid <abidsharier29@gmail.com>
2025-05-13 14:58:00 -0400

    dictionary res

--- Dicnfile.py
+++ Dicnfile.py
@@ -16,6 +16,28 @@ for line in fh:
 print(dix)
 print (count)
 for k,v in dix.items():
-    print("Name:",k,"Times:",v)
-"""
+    print("Name:",k,"Times:",v)"""
 
+name = input("Enter file:") or "mbox-short.txt"
+try:
+    fh= open(name)
+except:
+    print("Wrong file name.")
+    quit()
+dix={}
+for line in fh:
+    if not line.startswith("From "):
+        continue
+    else:
+        words=line.rstrip().split()
+        dix[words[1]]=dix.get(words[1],0)+1
+print(dix)
+most=0
+keyy=None
+for k,v in dix.items():
+    if v>most :
+        most = v
+        keyy = k
+print(keyy,most)
+        
+        
